f906c942505198f361ef35cef6061361a095cde4
Stephen <sjgowen@gmail.com>
2025-01-13 13:49:54 +0000

    Removed WinningDirection from WinningPlay as it is not used.

--- Connect456/Data/WinningPlay.cs
+++ Connect456/Data/WinningPlay.cs
@@ -5,6 +5,5 @@ namespace Connect456.Data;
 public class WinningPlay
 {
     public List<string> WinningMoves { get; set; }
-    public EvaluationDirection WinningDirection { get; set; }
     public PieceColor WinningColor { get; set; }
 }
